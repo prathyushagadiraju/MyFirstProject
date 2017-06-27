acbad44d89a87a789980f115a158bd8a9338ff01
Naman Verma <v-naverm@microsoft.com>
2017-06-27 13:49:33 -0700

    commit

---
+++ WebApplication/WebApplication/GetOutputScript.ps1
@@ -0,0 +1,28 @@
+#get the most recent deployment for the resource group
+$lastRgDeployment = Get-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName |
+    Sort Timestamp -Descending |
+        Select -First 1        
+
+if(!$lastRgDeployment)
+{
+    throw "Resource Group Deployment could not be found for '$resourceGroupName'."
+}
+
+$deploymentOutputParameters = $lastRgDeployment.Outputs
+
+if(!$deploymentOutputParameters)
+{
+    throw "No output parameters could be found for the last deployment of '$resourceGroupName'."
+}
+
+$outputParameter = $deploymentOutputParameters.Item($rgDeploymentOutputParameterName)
+
+if(!$outputParameter)
+{
+    throw "No output parameter could be found with the name of '$rgDeploymentOutputParameterName'."
+}
+
+$outputParameterValue  = $outputParameter.Value
+
+# From here, use $outputParameterValue, for example:
+Write-Host "##vso[task.setvariable variable=$rgDeploymentOutputParameterName;]$outputParameterValue"
